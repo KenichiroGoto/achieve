c2d17db89365df465c93de4a0624250b79423475
後藤 研一郎 <gotokenichiro@gotous-MacBook-Pro.local>
2017-03-24 21:15:01 +0900

    add action mailer for contract notice

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,12 @@ class NoticeMailer < ApplicationMailer
     mail to: "rage0776@gmail.com",
       subject:'【Achieve】ブログが投稿されました'
   end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: "#{contact.email}",
+      subject:'【Achieve】問い合わせが投稿されました'
+  end
+
 end
