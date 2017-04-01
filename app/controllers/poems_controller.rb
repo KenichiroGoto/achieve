91b4bf0cb5a5562c25ae7fed1dcceb7d09d33ba0
後藤研一郎 <gotokenichiro@gotoukenichirounoMacBook-Pro.local>
2017-04-01 21:41:34 +0900

    add poems index and show by apisample

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,5 +3,10 @@ class PoemsController < ApplicationController
   def index
     @poems = Poem.all
   end
-  
+
+  # add show action for normal gate of dive14_2
+  def show
+    @poem = Poem.find(params[:id])
+  end
+
 end
